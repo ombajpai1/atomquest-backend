ed0bca55246fd38896da161b895cdf80d2b3a58e
Om Bajpai <ombajpaiofficial@gmail.com>
2026-05-18 00:13:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app = FastAPI(title="AtomQuest Hackathon - Goal Tracker", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174","https://atomquest-frontend-orpin.vercel.app/login"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
